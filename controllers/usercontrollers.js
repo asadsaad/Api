2e106e82f2603264945e4a11cd4e54f5e8e9450c
asadsaad <asadxd55558@gmail.com>
2023-01-16 16:19:43 +0500

    USrr firx

--- controllers/usercontrollers.js
+++ controllers/usercontrollers.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
+const User = require("../models/usermodel");
 const bcrypt = require("bcrypt");
 
 exports.signup = async (req, res) => {
