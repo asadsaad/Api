d07f6d72cb7aba4d808acaf5d5b67a5d8f5d9be5
asadsaad <asadxd55558@gmail.com>
2023-01-16 16:17:20 +0500

    Fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const userRoutes = require("./Routes/userRoutes");
+const userRoutes = require("./routes/userroutes");
 const productRoutes = require("./routes/productroutes");
 const cartRoutes = require("./routes/cartroutes");
 require("dotenv").config();
